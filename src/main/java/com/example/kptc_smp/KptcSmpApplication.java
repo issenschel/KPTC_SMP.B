b046b4724887c699acb4ae2680991d49e3605143
heaven-wanderer <luckystqx@gmail.com>
2024-10-20 17:42:38 +0300

    Проект

---
+++ src/main/java/com/example/kptc_smp/KptcSmpApplication.java
@@ -0,0 +1,13 @@
+package com.example.kptc_smp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KptcSmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KptcSmpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kptc_smp/KptcSmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kptc_smp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KptcSmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
