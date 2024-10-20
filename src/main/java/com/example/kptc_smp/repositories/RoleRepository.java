315ad115bfea8f99d029bbda31b6364b59b17e12
heaven-wanderer <luckystqx@gmail.com>
2024-10-20 22:32:15 +0300

    Разработка JWT Token -Добавил DTO -Добавил репозитории -Добавил немного сервисов

---
+++ src/main/java/com/example/kptc_smp/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.example.kptc_smp.repositories;
+
+import com.example.kptc_smp.entitys.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
